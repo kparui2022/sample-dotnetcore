8e70ba8b40ff3636863238cfc4282352dc856ca5
kparui2022 <112687746+kparui2022@users.noreply.github.com>
2023-02-02 00:32:42 +0530

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -2,4 +2,4 @@
 printf "Shanti Chacha "
 #scp -o StrictHostKeyChecking=no *.* ubuntu@65.1.84.220:/home/ubuntu/ssh_key
 #scp -oStrictHostKeyChecking=no
-scp -i aws-access-key.pem *.* ubuntu@43.204.25.53:/home/ubuntu/cicd
+scp -i aws-access-key.pem *.* ubuntu@13.127.151.27:/home/ubuntu/cicd
