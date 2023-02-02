3d5a6070cb425779a9dcbbaaf3d6b18ab1362b2f
kparui2022 <112687746+kparui2022@users.noreply.github.com>
2023-02-02 11:07:41 +0530

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -i aws-access-key.pem /var/lib/jenkins/workspace/vue-js-test/app/dist/ ubuntu@13.235.0.2/var/www/html/vue-js-test/
+scp -i aws-access-key.pem /app/dist/ ubuntu@13.235.0.2/var/www/html/vue-js-test/
