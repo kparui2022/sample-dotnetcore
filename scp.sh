bee2ff2d6e9243089279b0576998f1ba11c394a2
kparui2022 <kaushik.p@divii.com>
2023-02-02 15:00:37 +0530

    basic update

--- scp.sh
+++ scp.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-printf "Shanti Chacha "
-#scp -o StrictHostKeyChecking=no *.* ubuntu@65.1.84.220:/home/ubuntu/ssh_key
-#scp -oStrictHostKeyChecking=no
-scp -i aws-access-key.pem *.* ubuntu@13.235.0.2:/home/ubuntu/cicd
+scp -i aws-access-key.pem app/dist/ ubuntu@13.235.0.2:/var/www/html/vue-js-test/
