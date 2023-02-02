1eaf046dc827a934683767ac366b6993542060e8
kparui2022 <kaushik.p@divii.com>
2023-02-02 10:28:49 +0530

    clear repo

--- scp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-printf "Shanti Chacha "
-#scp -o StrictHostKeyChecking=no *.* ubuntu@65.1.84.220:/home/ubuntu/ssh_key
-#scp -oStrictHostKeyChecking=no
-scp -i aws-access-key.pem *.* ubuntu@13.235.0.2:/home/ubuntu/cicd
