931392ff548156ec954d8967e209ec084696b353
kparui2022 <kaushik.p@divii.com>
2023-02-03 20:33:57 +0530

    Update before-deploy.sh

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ssh -i aws-access-key.pem ubuntu@13.127.196.123
-sudo su
-chown -R ubuntu:ubuntu /var/www/11-db-demo/
-chmod -R 777 /var/www/11-db-demo/
-rm -rf /var/www/11-db-demo/* .*
-exit
+#ssh -i aws-access-key.pem ubuntu@13.127.196.123
+#sudo su
+#chown -R ubuntu:ubuntu /var/www/11-db-demo/
+#chmod -R 777 /var/www/11-db-demo/
+#rm -rf /var/www/11-db-demo/* .*
+#exit
+sshpass -p "Ol635&zxNxfu" ssh -t root@13.127.196.123 sudo sh /var/www/jenkins-after-build.sh
