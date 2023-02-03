04cac7ae38046eb937947855eea36850f77ffd67
kparui2022 <kaushik.p@divii.com>
2023-02-03 19:49:37 +0530

    Create before-deploy.sh

---
+++ before-deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ssh -i aws-access-key.pem ubuntu@13.127.196.123 -yes
+sudo su
+chown -R ubuntu:ubuntu /var/www/11-db-demo/
+chmod -R 777 /var/www/11-db-demo/
+rm -rf /var/www/11-db-demo/* .*
