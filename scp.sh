ebab3dc6f1a9e40aee288e13bc9165665c91d9c0
kparui2022 <kaushik.p@divii.com>
2023-02-03 20:34:37 +0530

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-scp -i aws-access-key.pem app/dist/ ubuntu@13.235.0.2:/var/www/html/vue-js-test/
+#scp -i aws-access-key.pem app/dist/ ubuntu@13.235.0.2:/var/www/html/vue-js-test/
+sshpass -p "Ol635&zxNxfu" scp -r dist/* root@13.127.196.123:/var/www/11-db-demo/
