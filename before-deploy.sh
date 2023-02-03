49f1fe335453025981d1221a64770d19efb93006
kparui2022 <112687746+kparui2022@users.noreply.github.com>
2023-02-03 19:59:16 +0530

    Update before-deploy.sh

--- before-deploy.sh
+++ before-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh -i aws-access-key.pem ubuntu@13.127.196.123 -yes
+ssh -i aws-access-key.pem ubuntu@13.127.196.123
 sudo su
 chown -R ubuntu:ubuntu /var/www/11-db-demo/
 chmod -R 777 /var/www/11-db-demo/
