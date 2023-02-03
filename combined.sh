5aead8852e3ec481d20b301e8d51639554bd123b
kparui2022 <kaushik.p@divii.com>
2023-02-03 21:27:29 +0530

    12

--- combined.sh
+++ combined.sh
@@ -9,7 +9,7 @@ npm install
 npm run build-dev
 
 ##### Remote Server Document Directory Clean Script ######
-ssh -i aws-access-key.pem ubuntu@13.235.77.115 ./var/www/jenkins-after-build.sh
+ssh -i aws-access-key.pem ubuntu@13.235.77.115 ./jenkins-after-build.sh
 
 ##### Move Files From Jenkins To Destination Server ######
 scp -i aws-access-key.pem -r dist/* ubuntu@13.235.77.115:/var/www/html/11DB-test/
--- jenkins-after-build.sh
+++ jenkins-after-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-chown -R ubuntu:ubuntu /var/www/11-db-demo/
-chmod -R 777 /var/www/11-db-demo/
-rm -rf /var/www/11-db-demo/* .*
+sudo chown -R ubuntu:ubuntu /var/www/11-db-demo/
+sudo chmod -R 777 /var/www/11-db-demo/
+sudo rm -rf /var/www/11-db-demo/.*
+sudo rm -rf /var/www/11-db-demo/*
 exit
