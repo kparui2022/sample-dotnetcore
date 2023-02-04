b2199784899e63aa7da94420b25ed4d505e6b5f9
kparui2022 <kaushik.p@divii.com>
2023-02-04 16:42:30 +0530

    Update combine-without-scp.sh

--- combine-without-scp.sh
+++ combine-without-scp.sh
@@ -10,3 +10,9 @@ npm run build-dev
 
 ##### Remote Server Document Directory Clean Script ######
 ssh -i aws-access-key.pem ubuntu@3.110.165.7 ./jenkins-after-build.sh
+
+##### Copy Appspec.yml in Dist Folder for AWS-Deployment ######
+cd ..
+cd 11DB-vue-test/
+cp appspec.yml dist/
+cp -r scripts/ dist/
