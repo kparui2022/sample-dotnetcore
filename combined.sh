ccb4a0ac030c2f0ffb926b46bb471b74ec5d39a8
kparui2022 <kaushik.p@divii.com>
2023-02-04 15:31:15 +0530

    Update combined.sh

--- combined.sh
+++ combined.sh
@@ -3,7 +3,7 @@
 ##### PEM PERMISSION CHANGE ######
 chmod 600 aws-access-key.pem
 
-##### Jenkins Node Install ######
+##### Jenkins- Server Node Install ######
 npm config set legacy-peer-deps true
 npm install
 npm run build-dev
@@ -12,19 +12,6 @@ npm run build-dev
 ssh -i aws-access-key.pem ubuntu@3.110.165.7 ./jenkins-after-build.sh
 
 ##### Move Files From Jenkins To Destination Server ######
-cd ~
-cd /workspace/11DB-vue-test/
-chmod 600 aws-access-key.pem
-scp -i aws-access-key.pem -r dist/* ubuntu@3.110.165.7:/var/www/html/11-db-demo/
-
-
-
-
-#chmod 600 aws-access-key.pem
-#npm config set legacy-peer-deps true
-#npm install
-#npm run build-dev
-#ssh -i aws-access-key.pem ubuntu@3.110.165.7 ./jenkins-after-build.sh
-#cd ..
-#cd 11DB-vue-test/
-#scp -i aws-access-key.pem -r dist/* ubuntu@3.110.165.7:/var/www/11-db-demo
+cd ..
+cd 11DB-vue-test/
+scp -i aws-access-key.pem -r dist/* ubuntu@3.110.165.7:/var/www/11-db-demo
