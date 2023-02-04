ce45480bf10867ded3df863a68175599bbd42d3d
kparui2022 <kaushik.p@divii.com>
2023-02-04 14:10:01 +0530

    Update combined.sh

--- combined.sh
+++ combined.sh
@@ -9,7 +9,22 @@ npm install
 npm run build-dev
 
 ##### Remote Server Document Directory Clean Script ######
-ssh -i aws-access-key.pem ubuntu@13.235.77.115 ./jenkins-after-build.sh
+ssh -i aws-access-key.pem ubuntu@3.110.165.7 ./jenkins-after-build.sh
 
 ##### Move Files From Jenkins To Destination Server ######
-scp -i aws-access-key.pem -r dist/* ubuntu@13.235.77.115:/var/www/html/11DB-test/
+cd ~
+cd /workspace/11DB-vue-test/
+chmod 600 aws-access-key.pem
+scp -i aws-access-key.pem -r dist/* ubuntu@3.110.165.7:/var/www/html/11-db-demo/
+
+
+
+
+#chmod 600 aws-access-key.pem
+#npm config set legacy-peer-deps true
+#npm install
+#npm run build-dev
+#ssh -i aws-access-key.pem ubuntu@3.110.165.7 ./jenkins-after-build.sh
+#cd ..
+#cd 11DB-vue-test/
+#scp -i aws-access-key.pem -r dist/* ubuntu@3.110.165.7:/var/www/11-db-demo
